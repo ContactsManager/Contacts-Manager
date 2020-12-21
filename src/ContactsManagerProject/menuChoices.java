316caa8f4dc2dab309083ac59e9e0e7529d7d9c3
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-21 12:16:43 -0600

    factored template syntax printing

--- src/ContactsManagerProject/menuChoices.java
+++ src/ContactsManagerProject/menuChoices.java
@@ -1,4 +1,28 @@
 package ContactsManagerProject;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 public class menuChoices {
+    static Input input = new Input();
+    static Path p = Paths.get("src", "data", "contacts.txt");
+}
+
+public static void viewContacts() {
+    List<String> strList;
+    String name = "        Name";
+    String num = "     Number";
+    System.out.printf("%-18s | %s \n", name, num);
+    System.out.println("-----------------");
+    try {
+        strList = Files.readAllLines(p);
+        for ( int i = 0; i < strList.size(); i++){
+            System.out.printf((i + 1) + " : " + strList.get(i) + "\n");
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 }
