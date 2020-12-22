de4c420140768ca59db85331c26d660fa558a9e1
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-22 10:31:55 -0600

    fixed menu display switch statements

--- src/ContactsManagerProject/Input.java
+++ src/ContactsManagerProject/Input.java
@@ -16,15 +16,46 @@ public class Input {
         return scanner.nextLine();
     }
 
+//    public int getInt(){
+//        try{
+//            int userNumber = Integer.valueOf( scanner.nextLine() );
+//            return userNumber;
+//        } catch (Exception e){
+//            System.out.println("Invalid input.");
+//            return getInt();
+//        }
+//    }
+
+    public int getInt(int min, int max){
+        System.out.format("Choose number between %s and %s: \n", min, max);
+
+        try{
+            int userNum = Integer.parseInt( scanner.nextLine() );;
+
+            if ( userNum >= min && userNum  <= max){
+                return userNum;
+            }else {
+                System.out.format("invalid number, choose number between %s and %s: \n", min, max);
+                return getInt(min, max);
+            }
+        }catch (Exception e){
+            System.out.println("Not valid integer");
+            return getInt(min, max);
+        }
+    }
+
     public int getInt(){
-        Try{
-            int userNumber = Integer.valueOf( scanner.nextLine() );
-            return userNumber;
-        } catch (Exception e){
-            System.out.println("Invalid input.");
+        try{
+            int userNum = Integer.valueOf( scanner.nextLine() );
+            return userNum;
+
+        }catch (Exception e){
+            System.out.println("Not valid integer");
             return getInt();
         }
     }
+
+
 //  Will add to PSVM
 //    public char getChar(Scanner charScanner){
 //
--- src/ContactsManagerProject/mainMenu.java
+++ src/ContactsManagerProject/mainMenu.java
@@ -12,7 +12,8 @@ public class mainMenu extends menuChoices {
                     "4. Delete contact" +
                     "5. Exit\n");
             //This will run off of input
-            int userChoice = Input.getInt(1,5);
+            int userChoice = input.getInt(1, 5);
+
 
 
             switch(userChoice){
@@ -21,37 +22,41 @@ public class mainMenu extends menuChoices {
                     //Will run off of input
                     showContacts();
                     break;
+
                 case 2:
-                    //Will run off of input
-                    newContact();
-                    System.out.println("\n");
-                    //Shows added contact
-                    showContacts();
-                    System.out.println("\n");
-                    break;
-                case 3:
-                    //Will run off of input
-                    String findName = Input.getString("Choose a name:");
-                    System.out.println("\n");
-                    //CRUD
-                    searchContacts(findName);
-                    System.out.println("\n");
-                    break;
-                case 4:
-                    System.out.println("\n");
-                    showContacts();
-                    System.out.println("\n");
-                    //CRUD
-                    deleteContact();
-                    System.out.println("\n");
-                    //Verify deleted
-                    showContacts();
-                    System.out.println("\n");
-                    break;
-                case 5:
-                    stayOn = false;
-                    System.out.println("Critical ERROR! So long!");
-                    break;
+                    System.out.println("No worky");
+                            break;
+//                case 2:
+//                    //Will run off of input
+//                    newContact();
+//                    System.out.println("\n");
+//                    //Shows added contact
+//                    showContacts();
+//                    System.out.println("\n");
+//                    break;
+//                case 3:
+//                    //Will run off of input
+//                    String findName = Input.getString("Choose a name:");
+//                    System.out.println("\n");
+//                    //CRUD
+//                    searchContacts(findName);
+//                    System.out.println("\n");
+//                    break;
+//                case 4:
+//                    System.out.println("\n");
+//                    showContacts();
+//                    System.out.println("\n");
+//                    //CRUD
+//                    deleteContact();
+//                    System.out.println("\n");
+//                    //Verify deleted
+//                    showContacts();
+//                    System.out.println("\n");
+//                    break;
+//                case 5:
+//                    stayOn = false;
+//                    System.out.println("Critical ERROR! So long!");
+//                    break;
             }
         }while(stayOn);
     }
